dbc7a3f72008f1dc10a5c20bb7414c4734be6f1b
Alan M <Alan M>
2016-10-25 20:54:49 -0500

    Updated javascript and game object

--- gameJS.js
+++ gameJS.js
@@ -1,4 +1,4 @@
-	$(document).ready(function () {}
+	$(document).ready(function(){
 
 		//document ready calls the start game function - set scores to 0, reset letters guessed array, words guessed array reset
 
@@ -14,14 +14,29 @@
 
 		//guess check also calls ?win check and if that returns 1, move to update screen
 
-		var letter = '';
+		/*var letter = '';*/
 		
-		
-		game.validKeyCheck();
+game.gameStart();
 
+$(document).keyup(function(event) {
+		
+	var letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+	if(game.validKeyCheck(letter)){
+
+		game.guessCheck(letter);
+		
+
+	}
+
+	else{
+
+		return;
+	}
 
 	});
 
+});
 
 	var game = {
 
@@ -31,12 +46,9 @@
 		lettersGuessed: [],
 		createWord: function(){
 
-		currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-			              
-		             
-
-		}, // Tyler
-		winCheck: function () {
+			currentWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+		},
+		winCheck: function() {
 		//win defined as score = word length - 1
 		//assumes score gets set to 0 at game start and earn a point each correct guess
 
@@ -54,18 +66,13 @@
 			}
 			else return 1;
 		},  
-		guessCheck: function () {
+		guessCheck: function(index) {
 
-			$(".container").keyup(function(){
+			if(currentWord.indexOf(this) != -1){
 
-				if(currentWord.indexOf(this) != -1){
 
-						
-					}
-
-				}
-
-			})
+				game.updateScreen();
+			}
 
 		}, // Alan
 		updateScreen: function () {
@@ -93,25 +100,9 @@
 			$('#letters-guessed').html(lettersGuessed.join(","));
 
 		}, // Brandon
-
-		guessCheck: function () {
-
-			$(".container").keyup(function(){
-
-				if(currentWord.indexOf(this) != -1){
-
-						
-					}
-
-				}
-
-			})
-
-		}, // Alan
 		updateLettersGuessed: function () {}, // Alan
-		validKeyCheck: function () {
-			$(document).keyup(function(event) {
-				letter = String.fromCharCode(event.keyCode).toLowerCase();
+		validKeyCheck: function(letter) {
+			
 				console.log(letter);
 			});
 		}, // Harry
